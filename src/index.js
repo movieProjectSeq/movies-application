5c4f974256bb7d454d767a4d8a568b0a51ff6e7d
washingtondnavarro <washington.d.navarro@gmail.com>
2017-11-13 14:44:12 -0600

    -aWe add the loading spin and completed section one

--- src/index.js
+++ src/index.js
@@ -11,13 +11,30 @@ sayHello('World');
 const {getMovies} = require('./api.js');
 
 getMovies().then((movies) => {
-  console.log('Here are all the movies:');
+
+    let outputHtml = "";
+  // console.log('Here are all the movies:');
+
   movies.forEach(({title, rating, id}) => {
-      $('#test').text(`id#${id} - ${title} - rating: ${rating}`);
-      console.log(`id#${id} - ${title} - rating: ${rating}`);
+
+
+
+      // $('#test').text(`id#${id} - ${title} - rating: ${rating}`);
+      outputHtml += `<div id="${id}">`;
+      outputHtml += `<h2>Title: ${title} </h2>`;
+      outputHtml += `<h3> Rating: ${rating} </h3>`;
+      outputHtml += `</div>`
+
+      // console.log(`id#${id} - ${title} - rating: ${rating}`);
   });
+    $("#movies").removeClass("loader");
+
+
+    $("#movies").html(outputHtml)
+
 }).catch((error) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.');
   console.log(error);
+
 });
 
