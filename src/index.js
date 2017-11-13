f60fc73cfee3f1194b4ac5ac60aa0097804c052b
Cory Imel <cory.imel@gmail.com>
2017-11-13 12:32:27 -0600

    Initial

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 /**
  * es6 modules and imports
  */
+const $ = require('jquery');
 import sayHello from './hello';
 sayHello('World');
 
@@ -12,9 +13,11 @@ const {getMovies} = require('./api.js');
 getMovies().then((movies) => {
   console.log('Here are all the movies:');
   movies.forEach(({title, rating, id}) => {
-    console.log(`id#${id} - ${title} - rating: ${rating}`);
+      $('#test').text(`id#${id} - ${title} - rating: ${rating}`);
+      console.log(`id#${id} - ${title} - rating: ${rating}`);
   });
 }).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.')
+  alert('Oh no! Something went wrong.\nCheck the console for details.');
   console.log(error);
 });
+
