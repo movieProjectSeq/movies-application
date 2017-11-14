350bbaacf641034d65a19f9e86217ad8537e8ae4
Cory Imel <cory.imel@gmail.com>
2017-11-14 11:19:50 -0600

    added modal and api call functions

--- src/api.js
+++ src/api.js
@@ -1,9 +1,42 @@
 const $ = require("jquery");
+const blogPost = {title: 'Ajax Requests', body: 'Are a frustrating way to use JS!', id: 3};
+const url = 'api/movies/3';
+const options = {
+    method: 'PUT',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(blogPost),
+};
+
 module.exports = {
     getMovies: () => {
         $("#movies").addClass("loader");
         return fetch('/api/movies')
             .then(response => response.json());
+    },
+    addMovies: (input) => {
+        fetch(url, options)
+            .then(() => console.log('done'))
+            .catch(error => console.log('error'))
+    },
+    delMovies: (input) => {
+        fetch(url, options)
+            .then(() => console.log('done'))
+            .catch(error => console.log('error'))
+    },
+    editMovies: (input) => {
+        // const =
+        const options = {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(blogPost),
+        };
+        fetch(url, options)
+            .then()
+            .catch(error => console.log('error'))
     }
 };
 
--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
-/**
- * es6 modules and imports
- */
-const $ = require('jquery');
-let paginationOutTop =`<nav aria-label="Page navigation"><ul class="pagination"><li><a href="#" aria-label="Previous"><span aria-hidden="true">&laquo;</span></a></li>`;
-const paginationOutBottom = `<li><a href="#" aria-label="Next"><span aria-hidden="true">&raquo;</span></a></li></ul></nav>`;
-/**
- *
- *
- *
- * require style imports
- */
-const {getMovies} = require('./api.js');
 
-getMovies().then((movies) => {
-    let outputHtml = "";
+    /**
+     * es6 modules and imports
+     */
+    // const $ = require('jquery');
+    // const bs = require('bootstrap');
+    let paginationOutTop = `<nav aria-label="Page navigation"><ul class="pagination"><li><a href="#" aria-label="Previous"><span aria-hidden="true">&laquo;</span></a></li>`;
+    const paginationOutBottom = `<li><a href="#" aria-label="Next"><span aria-hidden="true">&raquo;</span></a></li></ul></nav>`;
+    /**
+     *
+     *
+     *
+     * require style imports
+     */
+    const {getMovies, delMovies, addMovies, editMovies} = require('./api.js');
+    getMovies().then((movies) => {
+        let outputHtml = "";
 
-  movies.forEach(({title, rating, id}) => {
+        movies.forEach(({title, rating, id}) => {
 
             outputHtml += `<div  id="boxInfo" class="well">`;
             outputHtml += `<div id="${id}">`;
@@ -26,16 +27,21 @@ getMovies().then((movies) => {
 
             paginationOutTop += `<li><a href="#${id}">${id}</a></li>`;
 
-  });
+        });
 
-    $('#pageNav').append(paginationOutTop + paginationOutBottom);
-    $('#movies').removeClass("loader").html(outputHtml);
-}).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.');
-  console.log(error);
-
-
-
-});
+        $('#pageNav').append(paginationOutTop + paginationOutBottom);
+        $('#movies').removeClass("loader").html(outputHtml);
+    }).catch((error) => {
+        alert('Oh no! Something went wrong.\nCheck the console for details.');
+        console.log(error);
 
 
+    });
+// addMovies();
+// delMovies();
+// editMovies();
+
+    $('#add-btn').click(function (e) {
+        e.preventDefault();
+         $('#add-modal').modal('show');
+    });
